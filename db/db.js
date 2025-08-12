3f9299d1f8d9b43379a7cf37de470eab74ebc0a2
Barbara <barbara.laisdorneles@gmail.com>
2025-08-12 19:49:08 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const knexLib = require('knex');
 const knexConfig = require('../knexfile.js');
 const env = process.env.NODE_ENV || 'development';
-const knex = knexLib(knexConfig[env]);
+const knex = knexLib(knexConfig);
 knex.on('query-error', (err) => {
   console.error('Knex query error:', err.message);
 });
